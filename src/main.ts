c5b377dff70a08d8d1fbd885b76c6b15d185c5b8
AnBANSAL-cmpm <abansal6@ucsc.edu>
2025-10-17 06:27:55 +0000

    Added logic for clearing the canvas and writing on the canvas with clicking and moving. Also added error msg (Step 2 done)

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,47 @@ canvas.width = 256;
 canvas.height = 256;
 canvas.id = "sketchup-canvas";
 document.body.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+if (!ctx) {
+  // Fill background
+  console.error("Failed to get 2D context");
+  throw new Error("Failed to get 2D context");
+}
+
+let isDrawing = false;
+
+canvas.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener("mouseleave", () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (!isDrawing || !ctx) return;
+
+  const rect = canvas.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+  ctx.arc(x, y, 4, 0, Math.PI * 2); //small circle
+  ctx.fill();
+});
+
+// Clear Button
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+clearButton.id = "clear-button";
+document.body.appendChild(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
